72cc8f72e07967cca209ecf6de7c158a98569eb5
Eduardo <43045722+edulanasca@users.noreply.github.com>
2024-04-07 03:37:51 -0500

    Wallet improvement (#29)
    * update: button improvement
    
    * update: burner wallet
    
    * fix: types
    
    * add mainnet sample contract
    
    ---------
    
    Co-authored-by: Eduardo <tech@smithii.io>
    Co-authored-by: 0xquantum3labs <contact@quantum3labs.com>

---
+++ packages/nextjs/components/scaffold-stark/CustomConnectButton/index.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+// @refresh reset
+import {Balance} from "../Balance";
+import {AddressInfoDropdown} from "./AddressInfoDropdown";
+import {AddressQRCodeModal} from "./AddressQRCodeModal";
+import {WrongNetworkDropdown} from "./WrongNetworkDropdown";
+import {useAutoConnect, useNetworkColor} from "~~/hooks/scaffold-stark";
+import {useTargetNetwork} from "~~/hooks/scaffold-stark/useTargetNetwork";
+import {getBlockExplorerAddressLink} from "~~/utils/scaffold-stark";
+import {useAccount, useNetwork} from "@starknet-react/core";
+import {Address} from "@starknet-react/chains";
+import {useState} from "react";
+import ConnectModal from "./ConnectModal";
+
+/**
+ * Custom Connect Button (watch balance + custom design)
+ */
+export const CustomConnectButton = () => {
+  useAutoConnect();
+  const networkColor = useNetworkColor();
+  const {targetNetwork} = useTargetNetwork();
+  const {address, status, chainId, ...props} = useAccount();
+  const {chain} = useNetwork();
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const blockExplorerAddressLink = address
+    ? getBlockExplorerAddressLink(targetNetwork, address)
+    : undefined;
+
+  const handleWalletConnect = () => {
+    setModalOpen(true);
+  }
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+  }
+
+  return (
+    status == "disconnected" ?
+      <>
+        <button className="btn btn-primary btn-sm" onClick={handleWalletConnect} type="button">
+          Connect Wallet
+        </button>
+        <ConnectModal isOpen={modalOpen} onClose={handleModalClose}/>
+      </>
+      : chainId !== targetNetwork.id ?
+        <WrongNetworkDropdown/>
+        :
+        <>
+          <div className="flex flex-col items-center mr-1">
+            <Balance address={address as Address} className="min-h-0 h-auto"/>
+            <span className="text-xs" style={{color: networkColor}}>{chain.name}</span>
+          </div>
+          <AddressInfoDropdown
+            address={address as Address}
+            displayName={""}
+            ensAvatar={""}
+            blockExplorerAddressLink={blockExplorerAddressLink}
+          />
+          <AddressQRCodeModal address={address as Address} modalId="qrcode-modal"/>
+        </>
+  );
+};
