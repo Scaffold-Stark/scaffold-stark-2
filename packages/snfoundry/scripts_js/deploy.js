785110e91f0494a629150a7113c3e1254566ba03
0xquantum3labs <contact@quantum3labs.com>
2024-04-09 01:22:29 +0800

    revert deploy scripts

--- packages/snfoundry/scripts_js/deploy.js
+++ packages/snfoundry/scripts_js/deploy.js
@@ -130,57 +130,65 @@ const deployScript = async () => {
   } = await deployContract(null, "HelloStarknet"); // can pass another argument for the exported contract name
   await deployContract(
     {
-      recipient: 1,
-    },
-    "Challenge0"
-  ); // simple storage receives an argument in the constructor
-
-  await deployContract(
-    {
       name: 1,
-      symbol: 2,
-      fixed_supply: 10,
-      recipient: "0x06072Bb27d275a0bC1deBf1753649b8721CF845B681A48443Ac46baF45769f8E",
     },
-    "Challenge1"
+    "SimpleStorage"
   );
+  // await deployContract(
+  //   {
+  //     recipient: 1,
+  //   },
+  //   "Challenge0"
+  // );
 
-  await deployContract(
-    {
-      base_uri: 1,
-      recipient: "0x06072Bb27d275a0bC1deBf1753649b8721CF845B681A48443Ac46baF45769f8E",
-      token_ids: 2,
-      values: 100,
-    },
-    "Challenge2"
-  );
+  // await deployContract(
+  //   {
+  //     name: 1,
+  //     symbol: 2,
+  //     fixed_supply: 10,
+  //     recipient:
+  //       "0x06072Bb27d275a0bC1deBf1753649b8721CF845B681A48443Ac46baF45769f8E",
+  //   },
+  //   "Challenge1"
+  // );
 
-  await deployContract(
-    {
-      public_key: "0x6e4fd4f9d6442e10cf8e20a799be3533be3756c5ea4d13e16a297d7d2717039",
-    },
-    "Challenge3"
-  );
+  // await deployContract(
+  //   {
+  //     base_uri: CallData.byteArrayFromString("https://example.com/"),
+  //     // recipient:
+  //     //   "0x06072Bb27d275a0bC1deBf1753649b8721CF845B681A48443Ac46baF45769f8E",
+  //     // token_ids: 2,
+  //     // values: 100,
+  //   },
+  //   "Challenge2"
+  // );
 
-  await deployContract(
-    {
-      voter_1:
-        "0x06072Bb27d275a0bC1deBf1753649b8721CF845B681A48443Ac46baF45769f8E",
-      voter_2:
-        "0x06072Bb27d275a0bC1deBf1753649b8721CF845B681A48443Ac46baF45769f8E",
-      voter_3:
-        "0x06072Bb27d275a0bC1deBf1753649b8721CF845B681A48443Ac46baF45769f8E",
-    },
-    "Vote"
-  );
-  await deployContract(
-    {
-      initial_owner: addAddressPadding(
-        "0x64b48806902a367c8598f4f95c305e8c1a1acba5f082d294a43793113115691"
-      ),
-    },
-    "Ownable"
-  ); // simple storage receives an argument in the constructor
+  // await deployContract(
+  //   {
+  //     public_key: "0x6e4fd4f9d6442e10cf8e20a799be3533be3756c5ea4d13e16a297d7d2717039",
+  //   },
+  //   "Challenge3"
+  // );
+
+  // await deployContract(
+  //   {
+  //     voter_1:
+  //       "0x06072Bb27d275a0bC1deBf1753649b8721CF845B681A48443Ac46baF45769f8E",
+  //     voter_2:
+  //       "0x06072Bb27d275a0bC1deBf1753649b8721CF845B681A48443Ac46baF45769f8E",
+  //     voter_3:
+  //       "0x06072Bb27d275a0bC1deBf1753649b8721CF845B681A48443Ac46baF45769f8E",
+  //   },
+  //   "Vote"
+  // );
+  // await deployContract(
+  //   {
+  //     initial_owner: addAddressPadding(
+  //       "0x64b48806902a367c8598f4f95c305e8c1a1acba5f082d294a43793113115691"
+  //     ),
+  //   },
+  //   "Ownable"
+  // ); // simple storage receives an argument in the constructor
 };
 
 deployScript()
