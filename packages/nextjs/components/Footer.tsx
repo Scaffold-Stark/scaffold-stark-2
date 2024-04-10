d488e173df936e723139385224bb76e21f933ad4
karlavasquez8 <karlavasquez817@gmail.com>
2024-04-10 11:12:50 -0500

    merge main

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -1,25 +1,12 @@
 import React from "react";
-import Link from "next/link";
-// import { devnet } from "viem/chains";
-import {
-  CurrencyDollarIcon,
-  MagnifyingGlassIcon,
-} from "@heroicons/react/24/outline";
 import { HeartIcon } from "@heroicons/react/24/outline";
 import { SwitchTheme } from "~~/components/SwitchTheme";
 import { BuidlGuidlLogo } from "~~/components/assets/BuidlGuidlLogo";
-// import { Faucet } from "~~/components/scaffold-eth";
-// import { useTargetNetwork } from "~~/hooks/scaffold-eth/useTargetNetwork";
-// import { useGlobalState } from "~~/services/store/store";
 
 /**
  * Site footer
  */
 export const Footer = () => {
-  //   const nativeCurrencyPrice = useGlobalState(
-  //     (state) => state.nativeCurrencyPrice
-  //   );
-  //   const { targetNetwork } = useTargetNetwork();
   const isLocalNetwork = false;
 
   return (
@@ -27,27 +14,6 @@ export const Footer = () => {
       <div>
         <div className="fixed flex justify-between items-center w-full z-10 p-4 bottom-0 left-0 pointer-events-none">
           <div className="flex flex-col md:flex-row gap-2 pointer-events-auto">
-            {/* {nativeCurrencyPrice > 0 && (
-              <div>
-                <div className="btn btn-primary btn-sm font-normal gap-1 cursor-auto">
-                  <CurrencyDollarIcon className="h-4 w-4" />
-                  <span>{nativeCurrencyPrice}</span>
-                </div>
-              </div>
-            )}
-            {isLocalNetwork && (
-              <>
-                <Faucet />
-                <Link
-                  href="/blockexplorer"
-                  passHref
-                  className="btn btn-primary btn-sm font-normal gap-1"
-                >
-                  <MagnifyingGlassIcon className="h-4 w-4" />
-                  <span>Block Explorer</span>
-                </Link>
-              </>
-            )} */}
           </div>
           <SwitchTheme
             className={`pointer-events-auto ${
