3adf4d6c20b5eb9ba93632b5e85a4fa9299d49d6
karlavasquez8 <karlavasquez817@gmail.com>
2024-04-15 17:59:05 -0500

    fix:title style

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
     <div>
       <div className="w-full flex items-center justify-center flex-col bg-landing gap-10 bg-base-100 text-primary sm:gap-3 ">
         <div className="w-full flex flex-col items-center gap-10 sm:gap-5">
-          <div className="w-full flex items-center justify-center flex-col gap-10 sm:px-[20px] sm:gap-5 sm:text-[12px]">
+          <div className="w-full flex items-center justify-center flex-col gap-10 sm:px-[20px] sm:gap-5 sm:text-[12px] max-w-[600px] ">
             <Image
               src={"/Starknet-icon.svg"}
               alt={"icon starknet"}
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
             <span className="text-center">
               Learn how to build on Starknet; the superpowers and the gotchas.
             </span>
-            <h1 className="text-7xl font-black text-center max-w-[500px] font-['Genos'] sm:text-6xl">
+            <h1 className="text-8xl font-black text-center sm:text-6xl font-['system-ui']">
               SPEEDRUN STARKNET
             </h1>
           </div>
@@ -66,8 +66,8 @@ const Home: NextPage = () => {
         <div className=" bg-ft-join flex justify-center bg-secondary-content text-secondary sm:h-[350px]">
           <div className="w-full px-[20px] flex justify-center">
             <div className="max-w-[1280px] flex justify-around flex-col w-full border-l-[5px] border-base-300 sm:justify-start sm:items-center sm:border-l-[3px] lg:border-l-[3px] pt-[20px]">
-              <div className="bg-banner-join flex justify-center h-[130px] w-full text-secondary font-black text-6xl items-center sm:text-3xl sm:h-[80px]">
-                <span className="font-['Genos'] font-black">
+              <div className="bg-banner-join flex justify-center h-[130px] w-full text-secondary font-black text-7xl items-center sm:text-3xl sm:h-[80px]">
+                <span className="font-black font-['system-ui']">
                   JOiN BUiLDGUiLD
                 </span>
               </div>
