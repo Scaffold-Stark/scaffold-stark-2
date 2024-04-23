1adbff390d70de734e01116fd8a86d9807cf80e3
GianMarco <gian.alarcon@unmsm.edu.pe>
2024-04-23 21:22:11 +0700

    Feat(Challenge 0): Implement Enumerable feature (#73)
    * Feat:  add fn token_of_owner_by_index
    
    * Feat(Ennumerable): Add owned_tokens, owned_tokens_index variables
    
    * Feat(Ennumerable): Implement fn _before_token_transfer()
    
    * Feat(Enumerable): Add all_tokens mapping

--- packages/snfoundry/scripts_js/deploy.js
+++ packages/snfoundry/scripts_js/deploy.js
@@ -13,21 +13,21 @@ const deployScript = async () => {
   //   "SimpleStorage"
   // );
 
-  // await deployContract(
-  //   {
-  //     owner:
-  //       "0x4b3f4ba8c00a02b66142a4b1dd41a4dfab4f92650922a3280977b0f03c75ee1",
-  //   }, // last account in devnet accounts
-  //   "Challenge0"
-  // );
+  await deployContract(
+    {
+      owner:
+        "0x4b3f4ba8c00a02b66142a4b1dd41a4dfab4f92650922a3280977b0f03c75ee1",
+    }, // last account in devnet accounts
+    "Challenge0"
+  );
 
-  const values = {
-      classHash: helloStarknetClassHash,
-      abi: helloStarknetAbi,
-      address: ContractAddress,
-    } = await deployContract(null, "ExampleExternalContract");
-  await deployContract({ external_contract_address: values.address, eth_contract_address: "0x49D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7" }
-    , "Challenge1");
+  // const values = {
+  //     classHash: helloStarknetClassHash,
+  //     abi: helloStarknetAbi,
+  //     address: ContractAddress,
+  //   } = await deployContract(null, "ExampleExternalContract");
+  // await deployContract({ external_contract_address: values.address, eth_contract_address: "0x49D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7" }
+  //   , "Challenge1");
   
 
   //await deployContract(null, "TransferETH");
@@ -42,16 +42,16 @@ const deployScript = async () => {
   //   "PresetERC20"
   // );
 
-  await deployContract(
-    {
-      base_uri: "https://example.com/",
-      recipient:
-        "0x64b48806902a367c8598f4f95c305e8c1a1acba5f082d294a43793113115691",
-      token_ids: [2],
-      values: [100],
-    },
-    "PresetERC1155"
-  );
+  // await deployContract(
+  //   {
+  //     base_uri: "https://example.com/",
+  //     recipient:
+  //       "0x64b48806902a367c8598f4f95c305e8c1a1acba5f082d294a43793113115691",
+  //     token_ids: [2],
+  //     values: [100],
+  //   },
+  //   "PresetERC1155"
+  // );
 
   // await deployContract(
   //   {
