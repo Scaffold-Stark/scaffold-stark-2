68e033ee0c0869b4a7c60d82dfcdf9ae3a67fbbe
Nadai2010 <nadai2010@proton.me>
2024-04-23 18:17:04 +0100

    Update Componente Header Logo "Svg"

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -1,13 +1,11 @@
-"use client";
-
 import React, { useCallback, useRef, useState } from "react";
-
 import { BugAntIcon } from "@heroicons/react/24/outline";
 import { useOutsideClick } from "~~/hooks/scaffold-stark";
 import { CustomConnectButton } from "~~/components/scaffold-stark/CustomConnectButton";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { FaucetButton } from "~~/components/scaffold-stark/FaucetButton";
+import HeaderLogo from "./HeaderLogo";
 
 type HeaderMenuLink = {
   label: string;
@@ -27,9 +25,6 @@ export const menuLinks: HeaderMenuLink[] = [
   },
 ];
 
-/**
- * Site header
- */
 export const Header = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const burgerMenuRef = useRef<HTMLDivElement>(null);
@@ -44,17 +39,9 @@ export const Header = () => {
       className={`lg:static top-0 navbar min-h-0 flex-shrink-0 justify-between z-20 px-0 sm:px-2 bg-base-100 ${pathname !== "/" ? "border-b border-[#1c2d49] bg-base-400" : ""}`}
     >
       <div className="pl-8 sm:pl-0">
-        {pathname !== "/" && (
-          <button onClick={() => (window.location.href = "/")}>
-            <Image
-              src={"/logo-header.svg"}
-              alt={"logo header"}
-              width={200}
-              height={65}
-              className="sm:w-[130px] "
-            />
-          </button>
-        )}
+        <button onClick={() => (window.location.href = "/")}>
+        <HeaderLogo /> 
+        </button>
       </div>
       <div className="navbar-end flex-grow pr-8 py-[8px] sm:pr-0 leading-7">
         <CustomConnectButton />
