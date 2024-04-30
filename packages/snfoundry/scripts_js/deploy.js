ce597bc0de87914ea46ee38377409f319ed74a86
GianMarco <gian.alarcon@unmsm.edu.pe>
2024-04-30 15:36:47 +0700

    Challenge 2: Smart contract implementation (#94)

--- packages/snfoundry/scripts_js/deploy.js
+++ packages/snfoundry/scripts_js/deploy.js
@@ -1,35 +1,60 @@
 const { deployer, deployContract } = require("./deploy_contract");
 const deployScript = async () => {
-  // const {
-  //   classHash: helloStarknetClassHash,
-  //   abi: helloStarknetAbi,
-  //   address: ContractAddress,
-  // } = await deployContract(null, "HelloStarknet"); // can pass another argument for the exported contract name
   // await deployContract(
   //   {
-  //     name: 1,
-  //   },
-  //   "SimpleStorage"
-  // );
-  // await deployContract(
-  //   {
-  //     owner: deployer.address, // the deployer address is the owner of the contract
+  //     owner:
+  //       "0x4b3f4ba8c00a02b66142a4b1dd41a4dfab4f92650922a3280977b0f03c75ee1", // last account in devnet accounts
   //   },
   //   "Challenge0"
   // );
-  const values = ({
-    classHash: helloStarknetClassHash,
-    abi: helloStarknetAbi,
-    address: ContractAddress,
-  } = await deployContract(null, "ExampleExternalContract"));
-  await deployContract(
+
+  // const values = {
+  //     classHash: helloStarknetClassHash,
+  //     abi: helloStarknetAbi,
+  //     address: ContractAddress,
+  //   } = await deployContract(null, "ExampleExternalContract");
+  // await deployContract({ external_contract_address: values.address, eth_contract_address: "0x49D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7" }
+  //   , "Challenge1");
+  
+  const your_token = await deployContract(
     {
-      external_contract_address: values.address,
-      eth_contract_address:
-        "0x49D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7",
+      name: "Gold",
+      symbol: "GLD",
+      fixed_supply: 2000,
+      recipient:
+        "0x64b48806902a367c8598f4f95c305e8c1a1acba5f082d294a43793113115691",
     },
-    "Challenge1"
-  );
+    "YourToken"
+    );
+  
+  const ch2 = await deployContract({
+    token_address: your_token.address,
+    eth_contract_address: "0x49D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7",
+    owner: "0x64b48806902a367c8598f4f95c305e8c1a1acba5f082d294a43793113115691"
+  }
+    , "Challenge2");
+  
+  // const { trasaction_hash } = await deployer.execute(
+  //   [
+  //     {
+  //       contractAddress: your_token.address,
+  //       calldata: [
+  //         ch2.address,
+  //         {
+  //           low: 300,
+  //           high: 0,
+  //         }
+  //       ],
+  //       entrypoint: "transfer",
+  //     }
+  //   ],
+  //   {
+  //     maxFee: 1e18
+  //   }
+  // );
+
+  // console.log("Transaction hash: ", trasaction_hash);
+  
   //await deployContract(null, "TransferETH");
   //   await deployContract(
   //   {
@@ -58,24 +83,7 @@ const deployScript = async () => {
   //   },
   //   "Challenge3"
   // );
-  // await deployContract(
-  //   {
-  //     voter_1:
-  //       "0x06072Bb27d275a0bC1deBf1753649b8721CF845B681A48443Ac46baF45769f8E",
-  //     voter_2:
-  //       "0x06072Bb27d275a0bC1deBf1753649b8721CF845B681A48443Ac46baF45769f8E",
-  //     voter_3:
-  //       "0x06072Bb27d275a0bC1deBf1753649b8721CF845B681A48443Ac46baF45769f8E",
-  //   },
-  //   "Vote"
-  // );
-  // await deployContract(
-  //   {
-  //     initial_owner:
-  //       "0x64b48806902a367c8598f4f95c305e8c1a1acba5f082d294a43793113115691",
-  //   },
-  //   "Ownable"
-  // ); // simple storage receives an argument in the constructor
+
 };
 
 deployScript()
