52a594ba85b9999b74b48a3f9b04b08b53e9e3f8
karlavasquez8 <karlavasquez817@gmail.com>
2024-04-30 20:20:11 -0500

    styles: Improve List

--- packages/nextjs/components/scaffold-stark/CustomConnectButton/Wallet.tsx
+++ packages/nextjs/components/scaffold-stark/CustomConnectButton/Wallet.tsx
@@ -1,48 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Connector } from "@starknet-react/core";
 import Image from "next/image";
 
 const Wallet = ({
-  handleConnectWallet,
-  connector,
-  loader,
-}: {
+                  handleConnectWallet,
+                  connector,
+                  loader,
+                }: {
   connector: Connector;
   loader: ({ src }: { src: string }) => string;
   handleConnectWallet: (
-    e: React.MouseEvent<HTMLButtonElement>,
-    connector: Connector,
+      e: React.MouseEvent<HTMLButtonElement>,
+      connector: Connector,
   ) => void;
 }) => {
   const isSvg = connector.icon.light?.startsWith("<svg");
+  const [clicked, setClicked] = useState(false);
 
   return (
-    <button
-      className="flex gap-4 items-center text-start p-[.2rem] hover:bg-outline-grey hover:rounded-[10px] transition-all cursor-pointer"
-      onClick={(e) => handleConnectWallet(e, connector)}
-    >
-      <div className="h-[2.2rem] w-[2.2rem] rounded-[5px]">
-        {isSvg ? (
-          <div
-            className="h-full w-full object-cover rounded-[5px]"
-            dangerouslySetInnerHTML={{
-              __html: connector.icon.light ?? "",
-            }}
-          />
-        ) : (
-          <Image
-            alt={connector.name}
-            loader={loader}
-            src={connector.icon.light!}
-            width={70}
-            height={70}
-            className="h-full w-full object-cover rounded-[5px]"
-          />
-        )}
-      </div>
-      <p className="flex-1 text-white">{connector.name}</p>
-    </button>
+      <button
+          className={`flex gap-4 items-center text-start p-[.2rem] hover:bg-outline-grey rounded-[10px] transition-all cursor-pointer hover:bg-base-100 pl-3 ${clicked ? 'bg-base-100' : ''}`}
+          onClick={(e) => {
+            setClicked(true);
+            handleConnectWallet(e, connector);
+          }}
+      >
+        <div className="h-[2.2rem] w-[2.2rem] rounded-[5px]">
+          {isSvg ? (
+              <div
+                  className="h-full w-full object-cover rounded-[5px]"
+                  dangerouslySetInnerHTML={{
+                    __html: connector.icon.light ?? "",
+                  }}
+              />
+          ) : (
+              <Image
+                  alt={connector.name}
+                  loader={loader}
+                  src={connector.icon.light!}
+                  width={70}
+                  height={70}
+                  className="h-full w-full object-cover rounded-[5px]"
+              />
+          )}
+        </div>
+        <p className="flex-1 text-primary-content font-medium">{connector.name}</p>
+      </button>
   );
 };
 
 export default Wallet;
+
