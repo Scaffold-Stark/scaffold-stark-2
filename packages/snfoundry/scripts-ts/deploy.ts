33f929f2c4188789d504c112864412359a87aeab
0xquantum3labs <contact@quantum3labs.com>
2024-05-18 20:29:54 +0800

    fix: multiple contract deployment:

--- packages/snfoundry/scripts-ts/deploy.ts
+++ packages/snfoundry/scripts-ts/deploy.ts
@@ -1,22 +1,25 @@
-import {
-  deployContract,
-  deployer,
-  resetDeploymentState,
-} from "./deploy-contract";
+import { deployContract, deployer, exportDeployments } from "./deploy-contract";
 
 const deployScript = async (): Promise<void> => {
-  resetDeploymentState();
-
   await deployContract(
     {
       owner: deployer.address, // the deployer address is the owner of the contract
     },
-    "YourContract"
+    "YourContract",
+    "YourContract1"
+  );
+  await deployContract(
+    {
+      owner: deployer.address, // the deployer address is the owner of the contract
+    },
+    "YourContract",
+    "YourContract2"
   );
 };
 
 deployScript()
   .then(() => {
+    exportDeployments();
     console.log("All Setup Done");
   })
   .catch(console.error);
