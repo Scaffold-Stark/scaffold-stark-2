e13b4845cb5c19480d850417d8ec906179c2319f
gianalarcon <gian.alarcon@unmsm.edu.pe>
2024-05-19 01:24:37 +0700

    Use YourContract

--- packages/snfoundry/scripts-ts/deploy.ts
+++ packages/snfoundry/scripts-ts/deploy.ts
@@ -1,11 +1,12 @@
 import { deployContract, deployer } from "./deploy-contract";
 
 const deployScript = async (): Promise<void> => {
-  await deployContract(null, "HelloStarknet");
+  await deployContract(
+    {
+      owner: deployer.address, // the deployer address is the owner of the contract
+    },
+    "YourContract"
+  );
 };
 
 deployScript()
-  .then(() => {
-    console.log("All Setup Done");
-  })
-  .catch(console.error);
