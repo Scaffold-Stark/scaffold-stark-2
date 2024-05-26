4ef3843e0a9b25ddbc88c9fcc765414e5da8eefd
Eduardo <edujlac@gmail.com>
2024-05-26 17:51:48 -0500

    fix int input

--- packages/nextjs/components/scaffold-stark/Input/utils.ts
+++ packages/nextjs/components/scaffold-stark/Input/utils.ts
@@ -6,42 +6,49 @@ export type CommonInputProps<T = string> = {
   disabled?: boolean;
 };
 
-export const SIGNED_NUMBER_REGEX = /^-?\d+\.?\d*$/;
-export const UNSIGNED_NUMBER_REGEX = /^\.?\d+\.?\d*$/;
+export const SIGNED_NUMBER_REGEX = /^-?\d*\.?\d*$/;
+export const UNSIGNED_NUMBER_REGEX = /^\d*\.?\d*$/;
 
-export const isValidInteger = (
-  dataType: string,
-  value: bigint | string,
-  strict = true,
-) => {
+export const isValidInteger = (dataType: string, value: string | bigint) => {
   const isSigned = dataType.split("::").pop()!.startsWith("i");
   const bitcount = Number(dataType.substring(isSigned ? 3 : 4));
 
-  let valueAsBigInt;
-  try {
-    valueAsBigInt = BigInt(value);
-  } catch (e) {}
-  if (typeof valueAsBigInt !== "bigint") {
-    if (strict) {
-      return false;
-    }
-    if (!value || typeof value !== "string") {
-      return true;
-    }
-    return isSigned
-      ? SIGNED_NUMBER_REGEX.test(value) || value === "-"
-      : UNSIGNED_NUMBER_REGEX.test(value);
-  } else if (!isSigned && valueAsBigInt < 0) {
+  if (
+    typeof value === "string" &&
+    !(isSigned
+      ? SIGNED_NUMBER_REGEX.test(value)
+      : UNSIGNED_NUMBER_REGEX.test(value))
+  ) {
     return false;
   }
+
+  let valueAsBigInt;
+  try {
+    const integerPart = value.toString().split(".")[0];
+    valueAsBigInt = BigInt(integerPart);
+  } catch (e) {
+    return false;
+  }
+
+  if (!isSigned && valueAsBigInt < 0) {
+    return false;
+  }
+
   const hexString = valueAsBigInt.toString(16);
   const significantHexDigits = hexString.match(/.*x0*(.*)$/)?.[1] ?? "";
-  return !(
-    significantHexDigits.length * 4 > bitcount ||
-    (isSigned &&
-      significantHexDigits.length * 4 === bitcount &&
-      parseInt(significantHexDigits.slice(-1)?.[0], 16) < 8)
-  );
+  if (significantHexDigits.length * 4 > bitcount) {
+    return false;
+  }
+
+  if (
+    isSigned &&
+    significantHexDigits.length * 4 === bitcount &&
+    parseInt(significantHexDigits.slice(-1)?.[0], 16) >= 8
+  ) {
+    return false;
+  }
+
+  return true;
 };
 
 // Treat any dot-separated string as a potential ENS name
