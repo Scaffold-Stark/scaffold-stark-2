a8c86a874e4a6e9386b5bf6093277a001cf610ef
Eduardo <edujlac@gmail.com>
2024-06-06 12:45:10 -0500

    update: remove try-catch in deploy

--- packages/snfoundry/scripts-ts/deploy.ts
+++ packages/snfoundry/scripts-ts/deploy.ts
@@ -1,23 +1,12 @@
 import { deployContract, deployer, exportDeployments } from "./deploy-contract";
 
 const deployScript = async (): Promise<void> => {
-  try {
-    await deployContract(
-      {
-        owner: deployer.address, // the deployer address is the owner of the contract
-      },
-      "YourContract"
-    );
-  } catch (error) {
-    console.log(error.message);
-    if (error.message.includes("fetch failed")) {
-      console.error(
-        "Couldn't deploy. Did you forget to run yarn chain? Or you can change target network in scaffold.config.ts"
-      );
-    }
-
-    throw error;
-  }
+  await deployContract(
+    {
+      owner: deployer.address, // the deployer address is the owner of the contract
+    },
+    "YourContract"
+  );
 };
 
 deployScript()
