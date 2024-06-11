ca2a9a4ac0106fde25f8e200472a241ccc7d5f69
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2024-06-11 17:30:48 +0200

    removing unused dependency

--- packages/nextjs/components/scaffold-stark/Address.tsx
+++ packages/nextjs/components/scaffold-stark/Address.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Address as AddressType } from "@starknet-react/chains";
