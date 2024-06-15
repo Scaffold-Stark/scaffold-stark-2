17ae37dda988b1c8099e35372227d4660d50e927
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2024-06-15 14:02:12 +0200

    feat: integrate starknetID (#169)

--- packages/nextjs/components/scaffold-stark/Address.tsx
+++ packages/nextjs/components/scaffold-stark/Address.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Address as AddressType } from "@starknet-react/chains";
@@ -12,8 +12,9 @@ import {
 } from "@heroicons/react/24/outline";
 import { useTargetNetwork } from "~~/hooks/scaffold-stark/useTargetNetwork";
 import { getBlockExplorerAddressLink } from "~~/utils/scaffold-stark";
-import { useStarkProfile } from "@starknet-react/core";
 import { BlockieAvatar } from "~~/components/scaffold-stark/BlockieAvatar";
+import { getStarknetPFPIfExists } from "~~/utils/profile";
+import useConditionalStarkProfile from "~~/hooks/useConditionalStarkProfile";
 
 type AddressProps = {
   address?: AddressType;
@@ -45,9 +46,10 @@ export const Address = ({
   const [ensAvatar, setEnsAvatar] = useState<string | null>();
   const [addressCopied, setAddressCopied] = useState(false);
   const checkSumAddress = address ? getChecksumAddress(address) : undefined;
-  //   const checkSumAddress = address ? address : undefined;
-
   const { targetNetwork } = useTargetNetwork();
+  const { data: profile } = useConditionalStarkProfile(address);
+
+  //   const checkSumAddress = address ? address : undefined;
 
   // TODO add starkprofile | not working on devnet now
   //const { data: fetchedProfile } = useStarkProfile({ address });
@@ -92,19 +94,32 @@ export const Address = ({
   return (
     <div className="flex items-center">
       <div className="flex-shrink-0">
-        <BlockieAvatar
-          address={checkSumAddress}
-          ensImage={ensAvatar}
-          size={(blockieSizeMap[size] * 24) / blockieSizeMap["base"]}
-        />
+        {getStarknetPFPIfExists(profile?.profilePicture) ? (
+          //eslint-disable-next-line @next/next/no-img-element
+          <img
+            src={profile?.profilePicture}
+            alt="Profile Picture"
+            className="rounded-full h-6 w-6"
+            width={24}
+            height={24}
+          />
+        ) : (
+          <BlockieAvatar
+            address={checkSumAddress}
+            ensImage={ensAvatar}
+            size={(blockieSizeMap[size] * 24) / blockieSizeMap["base"]}
+          />
+        )}
       </div>
       {disableAddressLink ? (
         <span className={`ml-1.5 text-${size} font-normal`}>
-          {displayAddress}
+          {profile?.name || displayAddress}
         </span>
       ) : targetNetwork.network === devnet.network ? (
         <span className={`ml-1.5 text-${size} font-normal`}>
-          <Link href={blockExplorerAddressLink}>{displayAddress}</Link>
+          <Link href={blockExplorerAddressLink}>
+            {profile?.name || displayAddress}
+          </Link>
         </span>
       ) : (
         <a
@@ -113,7 +128,7 @@ export const Address = ({
           href={blockExplorerAddressLink}
           rel="noopener noreferrer"
         >
-          {displayAddress}
+          {profile?.name || displayAddress}
         </a>
       )}
       {addressCopied ? (
