bb6363766dd45238f476507325160baa8192363d
BlackStarkGoku <165695008+BlackStarkGoku@users.noreply.github.com>
2024-06-19 18:17:48 +0200

    Update deploy contract file

--- packages/nextjs/Uikit/components/ui/wobble-card.tsx
+++ packages/nextjs/Uikit/components/ui/wobble-card.tsx
@@ -76,9 +76,12 @@ const Noise = () => {
     <div
       className="absolute inset-0 w-full h-full scale-[1.2] transform opacity-10 [mask-image:radial-gradient(#fff,transparent,75%)]"
       style={{
-        backgroundImage: "url(/noise.webp)",
+        backgroundImage: "url(noise.webp)",
         backgroundSize: "30%",
         borderRadius: "5rem",
+        position: "absolute",
+        inset: "0px",
+        opacity: 0.1,
       }}
     ></div>
   );
--- packages/snfoundry/scripts-ts/deploy.ts
+++ packages/snfoundry/scripts-ts/deploy.ts
@@ -7,10 +7,21 @@ const deployScript = async (): Promise<void> => {
   //    },
   //    "YourContract"
   //  );
-  await deployContract(null, "PragmaPrice");
-  await deployContract(null, "StarkPrice");
-  await deployContract(null, "BitcoinPrice");
-  await deployContract(null, "EtherPrice");
+  //await deployContract(null, "PragmaPrice");
+  //await deployContract(null, "StarkPrice");
+  await deployContract(
+    {
+      owner:
+        "0x034114DA641525543F43BBe8f931EA48158a90bd5c8af869d3eecf8364e04965",
+      end_date: 4102444800,
+      vote_date_limit: 4102444800,
+      reference_token_price: 6525086109850,
+      pragmaAddress:
+        "0x2a85bd616f912537c50a49a4076db02c00b29b2cdc8a197ce92ed1837fa875b",
+    },
+    "BitcoinPrice"
+  );
+  //await deployContract(null, "EtherPrice");
 };
 
 deployScript()
