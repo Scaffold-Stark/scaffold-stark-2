4126f15d9b8b8a8dcefc0c841e21ba1fc0ba6783
danielcdz <calde0601@gmail.com>
2024-07-12 22:49:27 -0600

    Create getConnectors function

--- packages/nextjs/services/web3/connectors.tsx
+++ packages/nextjs/services/web3/connectors.tsx
@@ -1,5 +1,22 @@
+import { argent, braavos } from "@starknet-react/core";
 import { getTargetNetworks } from "~~/utils/scaffold-stark";
+import { BurnerConnector } from "./stark-burner/BurnerConnector";
+import scaffoldConfig from "~~/scaffold.config";
 
 const targetNetworks = getTargetNetworks();
+
+export const  connectors  = getConnectors();
+
+export function getConnectors() {
+    const { targetNetworks } = scaffoldConfig;
+  
+    const connectors = [argent(), braavos()];
+  
+    if (targetNetworks.some(network => network.network === 'devnet')) {
+      connectors.push(new BurnerConnector());
+    }
+
+    return connectors;
+  }
 
 export const appChains = targetNetworks;
