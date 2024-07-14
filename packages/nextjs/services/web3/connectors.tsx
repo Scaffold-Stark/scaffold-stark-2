05f3604990e8ae9c55fe581ab439bf6bfc63c980
danielcdz <calde0601@gmail.com>
2024-07-14 10:41:43 -0600

    Shuffle connectors

--- packages/nextjs/services/web3/connectors.tsx
+++ packages/nextjs/services/web3/connectors.tsx
@@ -16,7 +16,15 @@ export function getConnectors() {
       connectors.push(new BurnerConnector());
     }
 
-    return connectors;
+    return shuffleArray(connectors);
+}
+
+function shuffleArray(array: any[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
   }
 
 export const appChains = targetNetworks;
