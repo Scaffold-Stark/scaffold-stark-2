3d96951b40e91035c700466efd601ed6402bf19b
Eduardo <edujlac@gmail.com>
2024-07-22 23:23:55 -0500

    update: error handling in deploy

--- packages/snfoundry/scripts-ts/deploy.ts
+++ packages/snfoundry/scripts-ts/deploy.ts
@@ -14,18 +14,4 @@ deployScript()
     exportDeployments();
     console.log("All Setup Done");
   })
-  .catch((error) => {
-    if (
-      typeof error.message === "string" &&
-      error.message.includes("no such file") &&
-      error.message.includes("compiled_contract_class")
-    ) {
-      const match = error.message.match(
-        /\/dev\/(.+?)\.compiled_contract_class/
-      );
-      const contractName = match ? match[1].split("_").pop() : "Unknown";
-      console.error(`The contract "${contractName}" doesn't exist`);
-    } else {
-      console.error(error);
-    }
-  });
+  .catch(console.error);
