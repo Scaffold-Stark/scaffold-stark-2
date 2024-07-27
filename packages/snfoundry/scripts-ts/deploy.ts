1208aca6a1461027e7f67119b115c319a8fadd01
0xquantum3labs <contact@quantum3labs.com>
2024-07-27 20:04:16 +0700

    chore: reset deployed contracts

--- packages/snfoundry/scripts-ts/deploy.ts
+++ packages/snfoundry/scripts-ts/deploy.ts
@@ -1,27 +1,15 @@
 import {
   deployContract,
-  deployer,
   executeDeployCalls,
   exportDeployments,
 } from "./deploy-contract";
 
 const deployScript = async (): Promise<void> => {
-  try {
   await deployContract({}, "Vars");
   await deployContract({}, "Structs");
-  await deployContract({}, "Tuples");
+  // await deployContract({}, "Tuples");
   await deployContract({}, "Complex");
   await deployContract({}, "ArraysSpans");
-  } catch (error) {
-    console.log(error.message);
-    if (error.message.includes("fetch failed")) {
-      console.error(
-        "Couldn't deploy. Did you forget to run yarn chain? Or you can change target network in scaffold.config.ts"
-      );
-    }
-
-    throw error;
-  }
 };
 
 deployScript()
