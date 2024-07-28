34d6ca61894c51141797930b5903e0bbbaaa70a8
0xquantum3labs <contact@quantum3labs.com>
2024-07-28 15:17:13 +0700

    chore: update OZ contract versions

--- packages/snfoundry/scripts-ts/deploy.ts
+++ packages/snfoundry/scripts-ts/deploy.ts
@@ -7,15 +7,16 @@ import {
 const deployScript = async (): Promise<void> => {
   await deployContract({}, "Vars");
   await deployContract({}, "Structs");
-  // await deployContract({}, "Tuples");
+  await deployContract({}, "Tuples");
   await deployContract({}, "Complex");
   await deployContract({}, "ArraysSpans");
 };
 
 deployScript()
   .then(() => {
-    exportDeployments();
-    executeDeployCalls().then(() => {});
+    executeDeployCalls().then(() => {
+      exportDeployments();
+    });
     console.log("All Setup Done");
   })
   .catch(console.error);
