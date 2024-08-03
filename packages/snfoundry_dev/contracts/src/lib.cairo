f64ead0c9756cddb63b103480bcb139b32ea5228
akinbola247 <akinbolakehinde247@gmail.com>
2024-08-03 12:59:15 +0100

    updates

--- packages/snfoundry_dev/contracts/src/lib.cairo
+++
@@ -1,5 +0,0 @@
-mod YourContract;
-#[cfg(test)]
-mod test {
-    mod TestContract;
-}
--- packages/snfoundry_dev/scripts-ts/types.ts
+++
@@ -1,8 +0,0 @@
-import { Account, RpcProvider } from "starknet";
-
-export type Networks = Record<"devnet" | "sepolia" | "mainnet", Network>;
-
-export type Network = {
-  provider: RpcProvider;
-  deployer: Account;
-};
