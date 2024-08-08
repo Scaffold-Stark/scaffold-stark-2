c94fa6985fdb0d17b75f0c5bf238245449ffea54
0xKenzman <112096641+Akinbola247@users.noreply.github.com>
2024-08-08 09:07:30 +0100

    Migrated Snfoundry package to support starknet foundry script (#223)
    Co-authored-by: Wolf <81079370+raizo07@users.noreply.github.com>

--- packages/nextjs/contracts/predeployedContracts.ts
+++ packages/nextjs/contracts/predeployedContracts.ts
@@ -3,7 +3,7 @@
  * You should not edit it manually or your changes might be overwritten.
  */
 const universalEthAddress =
-  "0x49D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7";
+  "0x049D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7";
 
 const universalStrkAddress =
   "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d";
