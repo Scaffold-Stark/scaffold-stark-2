5929c3f79d1a1fb19056118dff70e6ba6e2c20c8
Jemiiah <160767568+Jemiiah@users.noreply.github.com>
2024-08-09 10:28:10 +0000

    feat: yarn check

--- packages/nextjs/contracts/predeployedContracts.ts
+++ packages/nextjs/contracts/predeployedContracts.ts
@@ -215,7 +215,7 @@ const preDeployedContracts = {
           ],
         },
       ],
-      classHash: 0x046ded64ae2dead6448e247234bab192a9c483644395b66f2155f2614e5804b0
+      classHash: 0x046ded64ae2dead6448e247234bab192a9c483644395b66f2155f2614e5804b0,
     },
     Strk: {
       address: universalStrkAddress,
@@ -681,7 +681,7 @@ const preDeployedContracts = {
           ],
         },
       ],
-      classHash: 0x07f3777c99f3700505ea966676aac4a0d692c2a9f5e667f4c606b51ca1dd3420
+      classHash: 0x07f3777c99f3700505ea966676aac4a0d692c2a9f5e667f4c606b51ca1dd3420,
     },
     Strk: {
       address: universalStrkAddress,
