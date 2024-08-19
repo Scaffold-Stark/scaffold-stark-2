a30d60b6ba2b00c02a0668aa8a11e860b778c9b5
danielcdz <calde0601@gmail.com>
2024-08-19 12:13:24 -0600

    WIP add button to deploy wallet

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useCallback, useRef, useState, useEffect } from "react";
+import { Account, ec, json, stark, RpcProvider, hash, CallData } from 'starknet';
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -78,7 +79,7 @@ export const Header = () => {
   const isLocalNetwork = targetNetwork.id === devnet.id;
 
   const { provider } = useProvider();
-  const { address, status } = useAccount();
+  const { account, address, status } = useAccount();
   const [isDeployed, setIsDeployed] = useState(true);
 
   useEffect(() => {
@@ -142,9 +143,42 @@ export const Header = () => {
       </div>
       <div className="navbar-end flex-grow mr-4 gap-4">
         {status === "connected" && !isDeployed ? (
-          <span className="bg-[#8a45fc] text-[9px] p-1 text-white">
-            Wallet Not Deployed
-          </span>
+           <button
+           className={`rounded-[18px] btn-sm font-bold px-8 bg-btn-wallet`}
+           onClick={async () => {
+            console.log("Deploy wallet button clicked");
+            console.log("Provider:", provider.channel.nodeUrl);
+            console.log("wallet:", address)
+            console.log("account", account)
+
+            const accountAX = new Account(provider, '0x025231fc64a3440e60cE75b2B16DA0d93C3445cC1BA253eD2aC4e0Cc68537d', '0x00e560bd22ab96d801d62cff2d44ae71bd83b7dd2bca44a1d43f527183825e');
+
+            const AXConstructorCallData = CallData.compile({
+              owner: '0x025231fc64a3440e60cE75b2B16DA0d93445cC1BA253eD23BaC4e0Cc68537d',
+              guardian: '0',
+            });
+            const deployAccountPayload = {
+              classHash: '0x036078334509b514626504edc9fb252328d240e4e948bef8d0c08dff45927f',
+              constructorCalldata: AXConstructorCallData,
+              contractAddress: address,
+              addressSalt: '0x0377ca3f131349f9a79bfdb3280eaf07c2675bdb31a23c3ba955fa57ecea55',
+            };
+            
+            const response = await accountAX?.deployAccount(deployAccountPayload);
+
+            if (response && 'contract_address' in response && 'transaction_hash' in response) {
+              const { contract_address: AXdAth, transaction_hash: AXcontractFinalAddress } = response;
+              console.log('✅ ArgentX wallet deployed at:', AXcontractFinalAddress);
+            } else {
+              console.error('Failed to deploy account or response is undefined');
+            }
+
+
+          }}
+           type="button"
+         >
+           Deploy Wallet
+         </button>
         ) : null}
         <CustomConnectButton />
         {/* <FaucetButton /> */}
