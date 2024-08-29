84fd3ebde3436b959a1af8262276e2738a7e66a7
ASUS <buuhuynh2511@gmail.com>
2024-08-29 19:11:51 +0700

    fix issue264

--- packages/nextjs/hooks/scaffold-stark/useSwitchNetwork.ts
+++ packages/nextjs/hooks/scaffold-stark/useSwitchNetwork.ts
@@ -1,15 +1,27 @@
+
+const getChainId = (network: string): string => {
+  switch (network) {
+    case "mainnet":
+      return "SN_MAIN";
+    case "devnet":
+      return "SN_GOERLI";
+    default:
+      return `SN_${network.toUpperCase()}`;
+  }
+};
+
 export const useSwitchNetwork = () => {
   return {
     switchNetwork: async (network: string) => {
       // @ts-ignore
       if (window.starknet && window.starknet.isConnected) {
+        const chainId = getChainId(network);
+
         // @ts-ignore
         await window.starknet.request({
           type: "wallet_switchStarknetChain",
           params: {
-            chainId: `SN_${
-              network == "mainnet" ? "MAIN" : network.toUpperCase()
-            }`,
+            chainId: chainId,
           },
         });
       }
