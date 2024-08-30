3c5bb78cab0288d54fd40e266003713416766679
ASUS <buuhuynh2511@gmail.com>
2024-08-30 16:27:09 +0700

    commit to create draft PR

--- packages/nextjs/hooks/scaffold-stark/useSwitchNetwork.ts
+++ packages/nextjs/hooks/scaffold-stark/useSwitchNetwork.ts
@@ -1,15 +1,24 @@
+const getChainId = (network: string): string => {
+  switch (network) {
+    case "mainnet":
+      return "SN_MAIN";
+    case "devnet":
+      return "SN_GOERLI";
+    default:
+      return `SN_${network.toUpperCase()}`;
+  }
+};
+
 export const useSwitchNetwork = () => {
   return {
     switchNetwork: async (network: string) => {
-      // @ts-ignore
+      // @ts-expect-error: Assert that window.starknet exists and isConnected is a boolean
       if (window.starknet && window.starknet.isConnected) {
-        // @ts-ignore
+        // @ts-expect-error: Assert that window.starknet.request is a function and the parameters match
         await window.starknet.request({
           type: "wallet_switchStarknetChain",
           params: {
-            chainId: `SN_${
-              network == "mainnet" ? "MAIN" : network.toUpperCase()
-            }`,
+            chainId: getChainId(network),
           },
         });
       }
