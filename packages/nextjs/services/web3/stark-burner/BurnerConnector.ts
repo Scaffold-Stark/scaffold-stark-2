9b69a40eeac8dd7ac2682c5a811a0e972cc69ed5
metalboyrick <rsulisthio@gmail.com>
2024-09-08 22:28:36 +0700

    chore: handle burner connector error by showing messahe instead of user rejection

--- packages/nextjs/services/web3/stark-burner/BurnerConnector.ts
+++ packages/nextjs/services/web3/stark-burner/BurnerConnector.ts
@@ -113,7 +113,7 @@ export class BurnerConnector extends InjectedConnector {
             version: "0x3",
           });
       } catch (e) {
-        throw new UserRejectedRequestError();
+        throw e;
       }
     }
 
