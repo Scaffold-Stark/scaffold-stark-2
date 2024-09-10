cffba101d99e9ff6b8b95d3baf161530158c0f53
Richard <53423618+metalboyrick@users.noreply.github.com>
2024-09-10 10:00:15 +0700

    chore(v3): match v3 deployment script to main (#276)

--- packages/snfoundry/scripts-ts/deploy.ts
+++ packages/snfoundry/scripts-ts/deploy.ts
@@ -4,35 +4,57 @@ import {
   exportDeployments,
   deployer,
 } from "./deploy-contract";
-import { green, red } from "./helpers/colorize-log";
+import { green } from "./helpers/colorize-log";
 
+/**
+ * Deploy a contract using the specified parameters.
+ *
+ * @example (deploy contract with contructorArgs)
+ * const deployScript = async (): Promise<void> => {
+ *   await deployContract(
+ *     {
+ *       contract: "YourContract",
+ *       contractName: "YourContractExportName",
+ *       constructorArgs: {
+ *         owner: deployer.address,
+ *       },
+ *       options: {
+ *         maxFee: BigInt(1000000000000)
+ *       }
+ *     }
+ *   );
+ * };
+ *
+ * @example (deploy contract without contructorArgs)
+ * const deployScript = async (): Promise<void> => {
+ *   await deployContract(
+ *     {
+ *       contract: "YourContract",
+ *       contractName: "YourContractExportName",
+ *       options: {
+ *         maxFee: BigInt(1000000000000)
+ *       }
+ *     }
+ *   );
+ * };
+ *
+ *
+ * @returns {Promise<void>}
+ */
 const deployScript = async (): Promise<void> => {
-  try {
-    await deployContract(
-      {
-        owner: deployer.address,
-      },
-      "YourContract"
-    );
-  } catch (error) {
-    console.log(red(error.message));
-    if (error.message.includes("fetch failed")) {
-      console.error(
-        red(
-          "Couldn't deploy. Did you forget to run yarn chain? Or you can change target network in scaffold.config.ts"
-        )
-      );
-    }
-
-    throw error;
-  }
+  await deployContract({
+    contract: "YourContract",
+    constructorArgs: {
+      owner: deployer.address,
+    },
+  });
 };
 
 deployScript()
-  .then(() => {
-    executeDeployCalls().then(() => {
-      exportDeployments();
-    });
+  .then(async () => {
+    await executeDeployCalls();
+    exportDeployments();
+
     console.log(green("All Setup Done"));
   })
-  .catch((e) => console.error(red(e)));
+  .catch(console.error);
