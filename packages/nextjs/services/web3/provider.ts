b304965d0dcbe5289faf35c497e81728ae3464cc
metalboyrick <rsulisthio@gmail.com>
2024-09-19 15:54:27 +0800

    chore: use new provider selection mechanism

--- packages/nextjs/services/web3/provider.ts
+++ packages/nextjs/services/web3/provider.ts
@@ -10,15 +10,18 @@ const containsDevnet = (networks: readonly chains.Chain[]) => {
   return networks.filter((it) => it.id == chains.devnet.id).length > 0;
 };
 
-const provider =
-  scaffoldConfig.rpcProviderUrl == "" ||
-  containsDevnet(scaffoldConfig.targetNetworks)
-    ? publicProvider()
-    : jsonRpcProvider({
-        rpc: () => ({
+const provider = jsonRpcProvider({
+  rpc: () =>
+    // @ts-ignore cannot use ts expect error since it will complain if set to devnet
+    scaffoldConfig.targetNetworks[0].network === "devnet"
+      ? {
+          nodeUrl: "http://127.0.0.1:5050",
+          chainId: starknetChainId(scaffoldConfig.targetNetworks[0].id),
+        }
+      : {
           nodeUrl: scaffoldConfig.rpcProviderUrl,
           chainId: starknetChainId(scaffoldConfig.targetNetworks[0].id),
-        }),
-      });
+        },
+});
 
 export default provider;
