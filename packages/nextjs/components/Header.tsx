7aeeb58f6abc742454348010f79d2f50dddc6dab
root <root@Hp-pavilion>
2024-09-29 19:16:03 +0530

    fix checks

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -82,7 +82,7 @@ export const Header = () => {
   const burgerMenuRef = useRef<HTMLDivElement>(null);
   useOutsideClick(
     burgerMenuRef,
-    useCallback(() => setIsDrawerOpen(false), [])
+    useCallback(() => setIsDrawerOpen(false), []),
   );
   const { targetNetwork } = useTargetNetwork();
   const isLocalNetwork = targetNetwork.network === devnet.network;
