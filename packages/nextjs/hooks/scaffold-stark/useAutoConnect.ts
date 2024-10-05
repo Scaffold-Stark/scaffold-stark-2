bde267d3941b428ed4c0ad691954658824b6ae7e
Iwueseiter <156322726+Iwueseiter@users.noreply.github.com>
2024-10-05 09:50:49 +0000

    fix: lint

--- packages/nextjs/hooks/scaffold-stark/useAutoConnect.ts
+++ packages/nextjs/hooks/scaffold-stark/useAutoConnect.ts
@@ -1,19 +1,19 @@
-import { useReadLocalStorage } from "usehooks-ts";
-import { useEffect } from "react";
-import { useConnect } from "@starknet-react/core";
-import scaffoldConfig from "~~/scaffold.config";
-import type { BurnerConnector } from "~~/services/web3/stark-burner/BurnerConnector";
-import { burnerAccounts } from "~~/utils/devnetAccounts";
+import { useReadLocalStorage } from 'usehooks-ts';
+import { useEffect } from 'react';
+import { useConnect } from '@starknet-react/core';
+import scaffoldConfig from '~~/scaffold.config';
+import type { BurnerConnector } from '~~/services/web3/stark-burner/BurnerConnector';
+import { burnerAccounts } from '~~/utils/devnetAccounts';
 
 /**
  * Automatically connect to a wallet/connector based on config and prior wallet
  */
 export const useAutoConnect = (): void => {
   const savedConnector = useReadLocalStorage<{ id: string; ix?: number }>(
-    "lastUsedConnector",
+    'lastUsedConnector',
   );
 
-  const lastConnectionTime = useReadLocalStorage<number>("lastConnectionTime");
+  const lastConnectionTime = useReadLocalStorage<number>('lastConnectionTime');
 
   const { connect, connectors } = useConnect();
 
@@ -30,7 +30,7 @@ export const useAutoConnect = (): void => {
 
         if (connector) {
           if (
-            connector.id == "burner-wallet" &&
+            connector.id == 'burner-wallet' &&
             savedConnector?.ix !== undefined
           ) {
             (connector as BurnerConnector).burnerAccount =
