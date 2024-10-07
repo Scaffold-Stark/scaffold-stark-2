3948f6bea1851f822e66b8ef98287d763fb53844
Immanuelolivia1 <emmanueljuliet2019@gmail.com>
2024-10-07 23:35:56 +0100

    fixes

--- packages/nextjs/vitest.config.ts
+++ packages/nextjs/vitest.config.ts
@@ -5,13 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   test: {
-    // coverage: {  
-    //   reporter: ['text', 'json', 'html'],
-    //   include: ['src/**/*.ts', 'src/**/*.tsx', "jsdom"],
-    
-    // },
     environment: "jsdom",
-
   },
   resolve: {
     alias: {
@@ -19,5 +13,3 @@ export default defineConfig({
     },
   },
 });
-
-
