0357b459a09155402eaed477b11ee65554a4a5f9
iossocket <avx302@gmail.com>
2024-10-31 15:19:31 +0800

    add a new chain type only for forked mainnet

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -9,6 +9,27 @@ export type ScaffoldConfig = {
   autoConnectTTL: number;
 };
 
+const forkMainnet = {
+  id: BigInt("0x534e5f4d41494e"),
+  network: "devnet",
+  name: "Starknet Devnet",
+  nativeCurrency: {
+    address: "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+    name: "Ether",
+    symbol: "ETH",
+    decimals: 18
+  },
+  testnet: true,
+  rpcUrls: {
+    default: {
+      http: []
+    },
+    public: {
+      http: ["http://localhost:5050/rpc"]
+    }
+  }
+} as chains.Chain;
+
 const scaffoldConfig = {
   targetNetworks: [chains.devnet],
   // Only show the Burner Wallet when running on devnet
