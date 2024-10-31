e79f956f45f79e484428081af241772a91cc8ad7
iossocket <avx302@gmail.com>
2024-10-31 19:26:50 +0800

    add a new chain type only for forked mainnet (#338)

--- packages/nextjs/hooks/useConditionalStarkProfile.tsx
+++ packages/nextjs/hooks/useConditionalStarkProfile.tsx
@@ -2,13 +2,16 @@ import { useStarkProfile } from "@starknet-react/core";
 import * as chains from "@starknet-react/chains";
 import scaffoldConfig from "~~/scaffold.config";
 
+const shouldUseProfile = () => {
+  const set = new Set(["mainnet", "sepolia"]);
+  return set.has(scaffoldConfig.targetNetworks[0].network) && scaffoldConfig.targetNetworks[0].id !== chains.devnet.id;
+}
+
 const useConditionalStarkProfile = (address: chains.Address | undefined) => {
-  const shouldUseProfile =
-    scaffoldConfig.targetNetworks[0].id !== chains.devnet.id;
   // Conditional hooks are not recommended, but in this case, it's the best approach to avoid issues on devnet.
-  const profile = shouldUseProfile
+  const profile = shouldUseProfile()
     ? // eslint-disable-next-line react-hooks/rules-of-hooks
-      useStarkProfile({ address })
+    useStarkProfile({ address })
     : { data: undefined };
   return profile;
 };
