c4fc3dd31fcc5d047845aaf69310317ba91dda15
shantelpeters <elizabethogunrinde2015@gmail.com>
2024-12-14 15:00:54 +0000

    fix: useScaffoldContract write bug with external wallets #252

--- packages/nextjs/hooks/scaffold-stark/useScaffoldContract.ts
+++ packages/nextjs/hooks/scaffold-stark/useScaffoldContract.ts
@@ -1,7 +1,8 @@
 import { useDeployedContractInfo } from "~~/hooks/scaffold-stark";
 import { ContractName } from "~~/utils/scaffold-stark/contract";
-import { Contract } from "starknet";
+import { Contract, Abi } from "starknet";
 import { useProvider } from "@starknet-react/core";
+import { useMemo } from "react";
 
 export const useScaffoldContract = <TContractName extends ContractName>({
   contractName,
@@ -10,15 +11,29 @@ export const useScaffoldContract = <TContractName extends ContractName>({
 }) => {
   const { data: deployedContractData, isLoading: deployedContractLoading } =
     useDeployedContractInfo(contractName);
+  
   const { provider: publicClient } = useProvider();
-  let contract = undefined;
-  if (deployedContractData) {
-    contract = new Contract(
-      [...deployedContractData.abi],
+
+  const contract = useMemo(() => {
+    if (!deployedContractData) return undefined;
+
+    const contractInstance = new Contract(
+      deployedContractData.abi as Abi,
       deployedContractData.address,
-      publicClient,
+      publicClient
     );
-  }
+
+    const originalCall = contractInstance.call.bind(contractInstance);
+    contractInstance.call = async (method: string, ...args: any[]) => {
+      try {
+        return await originalCall(method, ...args, { parseResponse: false });
+      } catch (error) {
+        return originalCall(method, ...args);
+      }
+    };
+
+    return contractInstance;
+  }, [deployedContractData, publicClient]);
 
   return {
     data: contract,
