7a65992b3b68dc43472f86ee0ef63877144b3f8b
iossocket <avx302@gmail.com>
2024-12-31 19:11:38 +0800

    Fix Vercel build & deployment issue (#405)

--- packages/nextjs/next.config.mjs
+++ packages/nextjs/next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+import webpack from "webpack";
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -27,6 +29,11 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push("pino-pretty", "lokijs", "encoding");
+    config.plugins.push(
+      new webpack.NormalModuleReplacementPlugin(/^node:(.*)$/, (resource) => {
+        resource.request = resource.request.replace(/^node:/, "");
+      }),
+    );
     return config;
   },
 };
