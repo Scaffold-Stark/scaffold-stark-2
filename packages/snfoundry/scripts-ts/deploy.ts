047462981578c500bf58544af6c76cb9e863a47e
Buuh2511 <buuhuynh2511@gmail.com>
2025-01-08 16:10:52 +0700

    put declare as an example

--- packages/snfoundry/scripts-ts/deploy.ts
+++ packages/snfoundry/scripts-ts/deploy.ts
@@ -3,72 +3,73 @@ import {
   executeDeployCalls,
   exportDeployments,
   deployer,
-  declareContract,
 } from "./deploy-contract";
 import { green } from "./helpers/colorize-log";
 
 /**
- * First declare and then deploy a contract using the specified parameters.
- *
- * @example (deploy contract with contructorArgs)
- * const mainScript = async (): Promise<void> => {
+ * @example (declare contract)
+ * const declareScript = async (): Promise<void> => {
  *   await declareContract({
  *     contract: "YourContract",
  *     options: {}
  *   });
  *
- *   await deployContract({
- *     contract: "YourContract",
- *     contractName: "YourContractExportName",
- *     constructorArgs: {
- *       owner: deployer.address,
- *     },
- *     options: {
- *       maxFee: BigInt(1000000000000)
+ **/
+
+/**
+ * Deploy a contract using the specified parameters.
+ *
+ * @example (deploy contract with contructorArgs)
+ * const deployScript = async (): Promise<void> => {
+ *   await deployContract(
+ *     {
+ *       contract: "YourContract",
+ *       contractName: "YourContractExportName",
+ *       constructorArgs: {
+ *         owner: deployer.address,
+ *       },
+ *       options: {
+ *         maxFee: BigInt(1000000000000)
+ *       }
  *     }
- *   });
+ *   );
  * };
  *
  * @example (deploy contract without contructorArgs)
- * const mainScript = async (): Promise<void> => {
- *   await declareContract({
- *     contract: "YourContract",
- *     options: {}
- *   });
- *
- *   await deployContract({
- *     contract: "YourContract",
- *     contractName: "YourContractExportName",
- *     options: {
- *       maxFee: BigInt(1000000000000)
+ * const deployScript = async (): Promise<void> => {
+ *   await deployContract(
+ *     {
+ *       contract: "YourContract",
+ *       contractName: "YourContractExportName",
+ *       options: {
+ *         maxFee: BigInt(1000000000000)
+ *       }
  *     }
- *   });
+ *   );
  * };
+ *
  *
  * @returns {Promise<void>}
  */
-const mainScript = async (): Promise<void> => {
-  try {
-    await declareContract({
-      contract: "YourContract",
-      options: {},
-    });
-
-    await deployContract({
-      contract: "YourContract",
-      constructorArgs: {
-        owner: deployer.address,
-      },
-    });
-
-    await executeDeployCalls();
-    exportDeployments();
-    console.log(green("All Setup Done"));
-  } catch (error) {
-    console.error("Error in deployment process:", error);
-    process.exit(1); // exit with error so that no subsequent scripts are run
-  }
+const deployScript = async (): Promise<void> => {
+  await deployContract({
+    contract: "YourContract",
+    constructorArgs: {
+      owner: deployer.address,
+    },
+  });
 };
 
-// Execute the main script
-mainScript().catch(console.error);
+deployScript()
+  .then(async () => {
+    executeDeployCalls()
+      .then(() => {
+        exportDeployments();
+        console.log(green("All Setup Done"));
+      })
+      .catch((e) => {
+        console.error(e);
+        process.exit(1); // exit with error so that non subsequent scripts are run
+      });
+  })
+  .catch(console.error);
