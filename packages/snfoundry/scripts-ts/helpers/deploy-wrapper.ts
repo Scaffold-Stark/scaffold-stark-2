07810f7d348a90d25c4e252d89f8bc67d25f7342
metalboyrick <rsulisthio@gmail.com>
2025-01-17 20:29:38 +0700

    feat: count arguments outside defined options as invalid

--- packages/snfoundry/scripts-ts/helpers/deploy-wrapper.ts
+++ packages/snfoundry/scripts-ts/helpers/deploy-wrapper.ts
@@ -10,34 +10,49 @@ interface CommandLineOptions {
   fee?: string;
 }
 
-const argv = yargs(process.argv.slice(2))
-  .options({
-    network: { type: "string", default: "devnet" },
-    fee: { type: "string", choices: ["eth", "strk"], default: "eth" },
-    reset: {
-      type: "boolean",
-      description: "Do not reset deployments (keep existing deployments)",
-      default: true,
-    },
-  })
-  .demandOption(["network", "fee", "reset"])
-  .parseSync() as CommandLineOptions;
+function main() {
+  const argv = yargs(process.argv.slice(2))
+    .options({
+      network: { type: "string", default: "devnet" },
+      fee: { type: "string", choices: ["eth", "strk"], default: "eth" },
+      reset: {
+        type: "boolean",
+        description: "Do not reset deployments (keep existing deployments)",
+        default: true,
+      },
+    })
+    .demandOption(["network", "fee", "reset"])
+    .parseSync() as CommandLineOptions;
 
-// Set the NETWORK environment variable based on the --network argument
-process.env.NETWORK = argv.network || "devnet";
-process.env.FEE_TOKEN = argv.fee || "eth";
-process.env.NO_RESET = !argv.reset ? "true" : "false";
+  if (argv._.length > 0) {
+    console.error(
+      `❌ Invalid arguments, only --network, --fee, or --reset/--no-reset can be passed in`,
+    );
+    return;
+  }
 
-// Execute the deploy script without the reset option
-try {
-  execSync(
-    `cd contracts && scarb build && ts-node ../scripts-ts/deploy.ts` +
-      ` --network ${process.env.NETWORK}` +
-      ` --fee ${process.env.FEE_TOKEN}` +
-      ` --no-reset ${process.env.NO_RESET}` +
-      ` && ts-node ../scripts-ts/helpers/parse-deployments.ts && cd ..`,
-    { stdio: "inherit" },
-  );
-} catch (error) {
-  console.error("Error during deployment:", error);
+  // Set the NETWORK environment variable based on the --network argument
+  process.env.NETWORK = argv.network || "devnet";
+  process.env.FEE_TOKEN = argv.fee || "eth";
+  process.env.RESET = argv.reset ? "true" : "false";
+
+  console.log(process.env.NETWORK, process.env.FEE_TOKEN, process.env.NO_RESET);
+
+  // Execute the deploy script without the reset option
+  try {
+    execSync(
+      `cd contracts && scarb build && ts-node ../scripts-ts/deploy.ts` +
+        ` --network ${process.env.NETWORK}` +
+        ` --fee ${process.env.FEE_TOKEN}` +
+        ` --reset ${process.env.RESET}` +
+        ` && ts-node ../scripts-ts/helpers/parse-deployments.ts && cd ..`,
+      { stdio: "inherit" },
+    );
+  } catch (error) {
+    console.error("Error during deployment:", error);
+  }
+}
+
+if (require.main === module) {
+  main();
 }
