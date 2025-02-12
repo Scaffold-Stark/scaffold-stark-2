f1f89dbe0d21f45bc360396d0ed902b401b06fc7
iossocket <avx302@gmail.com>
2025-02-12 12:04:42 +0800

    add playwright

--- ui-automation/start.sh
+++ ui-automation/start.sh
@@ -23,3 +23,5 @@ done
 echo "Setup complete! You can now access:"
 echo "- Starknet Devnet: http://localhost:5050"
 echo "- NextJS App: http://localhost:3000"
+
+docker compose exec playwright npm run test
