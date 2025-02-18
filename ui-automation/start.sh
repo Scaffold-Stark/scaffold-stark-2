b29ea269a4b66c3b5fe28e81ab184bdcc2f6c57c
iossocket <avx302@gmail.com>
2025-02-18 22:40:54 +0800

    add snfoundry image to compile contracts

--- ui-automation/start.sh
+++ ui-automation/start.sh
@@ -16,9 +16,8 @@ while ! curl -s http://localhost:3000 > /dev/null; do
     sleep 1
 done
 
-# # 进入 nextjs 容器执行部署命令
-# echo "Deploying contracts..."
-# docker-compose exec nextjs yarn workspace @ss-2/snfoundry deploy
+echo "Deploying contracts..."
+docker compose exec snfoundry yarn deploy
 
 echo "Setup complete! You can now access:"
 echo "- Starknet Devnet: http://localhost:5050"
