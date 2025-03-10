180ae4d4c251a353da6688c01e9431049f0578c7
nightmarefox12 <carloshenriquez392004@gmail.com>
2025-03-10 07:23:39 -0400

    fix: Improve deployment file clearing script argument handling

--- packages/snfoundry/deployments/clear.mjs
+++ packages/snfoundry/deployments/clear.mjs
@@ -4,16 +4,19 @@ import path from "path";
 const k = ["devnet", "sepolia", "mainnet"];
 const args = process.argv.slice(2);
 
-if (args.some((x) => !x.includes("help"))) {
-  const match = k.some((x) => args[1]?.includes(x) ?? false);
+if (args.some((x) => !x.includes("help")) || args.length === 0) {
+  const match = k.some((x) => args[0]?.includes(x) ?? false);
 
   // Deletes every temporary file found.
   fs.readdir(".", (e, f) => {
     if (e) console.error("Error reading directory: ", e);
 
     let mf;
-    if (!match) mf = f.filter((x) => k.some((w) => x.startsWith(w)));
-    else mf = f.filter((x) => x.startsWith(args[0].substring(2)));
+    if (args.length === 0) mf = f.filter((x) => k.some((w) => x.startsWith(w)));
+    else
+      mf = f.filter((x) =>
+        x.startsWith(match ? args[0].substring(2) : args[1])
+      );
 
     for (let i = 0; i < mf.length; i++) {
       const f = path.join(".", mf[i]);
