cf1975627ec9cc9f45446df303f61a477b7de530
tu11aa <thanhtuhr01@gmail.com>
2025-04-16 17:26:01 +0700

    fix: only choose starknet-native as framework

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,14 @@
 import { Config, typedQuestion } from "./types";
 
 const config: Config = {
-  questions: [],
+  questions: [
+    typedQuestion({
+      type: "single-select",
+      name: "chain-type",
+      message: "What framework do you want to use?",
+      extensions: ["starknet-native"],
+      default: "starknet-native",
+    }),
+  ],
 };
 export default config;
