f98cd48dfb740b87b3fd7ba86cede722563adbed
tu11aa <75005519+tu11aa@users.noreply.github.com>
2025-04-22 00:28:29 +0700

    Fix: Fast Reload not working on testnet mode (#496)
    Co-authored-by: gianalarcon <gian@quantum3labs.com>

--- packages/nextjs/hooks/scaffold-stark/useTransactor.tsx
+++ packages/nextjs/hooks/scaffold-stark/useTransactor.tsx
@@ -7,11 +7,24 @@ import {
 } from "starknet";
 import { getBlockExplorerTxLink, notification } from "~~/utils/scaffold-stark";
 import { useTargetNetwork } from "./useTargetNetwork";
+import { useState, useEffect } from "react";
+import {
+  useSendTransaction,
+  UseSendTransactionResult,
+  useTransactionReceipt,
+  UseTransactionReceiptResult,
+} from "@starknet-react/core";
 
 type TransactionFunc = (
-  tx: () => Promise<InvokeFunctionResponse> | Promise<string>,
-  // | SendTransactionParameters,
+  tx: Call[],
+  withSendTransaction?: boolean,
 ) => Promise<string | undefined>;
+
+interface UseTransactorReturn {
+  writeTransaction: TransactionFunc;
+  transactionReceiptInstance: UseTransactionReceiptResult;
+  sendTransactionInstance: UseSendTransactionResult;
+}
 
 /**
  * Custom notification content for TXs.
@@ -43,19 +56,59 @@ const TxnNotification = ({
 /**
  * Runs Transaction passed in to returned function showing UI feedback.
  * @param _walletClient - Optional wallet client to use. If not provided, will use the one from useWalletClient.
- * @returns function that takes in transaction function as callback, shows UI feedback for transaction and returns a promise of the transaction hash
+ * @returns An object with the writeTransaction function, transaction status, and other transaction-related properties
  */
 export const useTransactor = (
   _walletClient?: AccountInterface,
-): TransactionFunc => {
+): UseTransactorReturn => {
   let walletClient = _walletClient;
   const { account, address, status } = useAccount();
   const { targetNetwork } = useTargetNetwork();
   if (walletClient === undefined && account) {
     walletClient = account;
   }
+  const sendTransactionInstance = useSendTransaction({});
 
-  return async (tx) => {
+  const [notificationId, setNotificationId] = useState<string | null>(null);
+  const [blockExplorerTxURL, setBlockExplorerTxURL] = useState<
+    string | undefined
+  >(undefined);
+  const [transactionHash, setTransactionHash] = useState<string | undefined>(
+    undefined,
+  );
+  const transactionReceiptInstance = useTransactionReceipt({
+    hash: transactionHash,
+  });
+  const { data: txResult, status: txStatus } = transactionReceiptInstance;
+
+  const resetStates = () => {
+    setTransactionHash(undefined);
+    setBlockExplorerTxURL(undefined);
+  };
+
+  useEffect(() => {
+    if (notificationId && txStatus && txStatus !== "pending") {
+      notification.remove(notificationId);
+    }
+    if (txStatus === "success") {
+      notification.success(
+        <TxnNotification
+          message="Transaction completed successfully!"
+          blockExplorerLink={blockExplorerTxURL}
+        />,
+        {
+          icon: "🎉",
+        },
+      );
+      resetStates();
+    }
+  }, [txResult]);
+
+  const writeTransaction = async (
+    tx: Call[],
+    withSendTransaction: boolean = true,
+  ): Promise<string | undefined> => {
+    resetStates();
     if (!walletClient) {
       notification.error("Cannot access account");
       console.error("⚡️ ~ file: useTransactor.tsx ~ error");
@@ -71,9 +124,9 @@ export const useTransactor = (
       notificationId = notification.loading(
         <TxnNotification message="Awaiting for user confirmation" />,
       );
-      if (typeof tx === "function") {
+      if (tx != null && withSendTransaction) {
         // Tx is already prepared by the caller
-        const result = await tx();
+        const result = await sendTransactionInstance.sendAsync(tx);
         if (typeof result === "string") {
           transactionHash = result;
         } else {
@@ -134,11 +187,14 @@ export const useTransactor = (
         throw new Error("Incorrect transaction passed to transactor");
       }
 
+      setTransactionHash(transactionHash);
+
       notification.remove(notificationId);
 
       const blockExplorerTxURL = networkId
         ? getBlockExplorerTxLink(targetNetwork.network, transactionHash)
         : "";
+      setBlockExplorerTxURL(blockExplorerTxURL);
 
       notificationId = notification.loading(
         <TxnNotification
@@ -146,18 +202,7 @@ export const useTransactor = (
           blockExplorerLink={blockExplorerTxURL}
         />,
       );
-
-      notification.remove(notificationId);
-
-      notification.success(
-        <TxnNotification
-          message="Transaction completed successfully!"
-          blockExplorerLink={blockExplorerTxURL}
-        />,
-        {
-          icon: "🎉",
-        },
-      );
+      setNotificationId(notificationId);
     } catch (error: any) {
       if (notificationId) {
         notification.remove(notificationId);
@@ -174,5 +219,11 @@ export const useTransactor = (
     }
 
     return transactionHash;
+  };
+
+  return {
+    writeTransaction,
+    transactionReceiptInstance,
+    sendTransactionInstance,
   };
 };
