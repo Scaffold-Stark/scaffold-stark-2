0ab3ff72bd7cf4a113919d9a553a95b26d619d2f
metalboyrick <rsulisthio@gmail.com>
2025-05-02 17:37:16 +0700

    chore: skip contract formatting with scarb

--- src/tasks/prettier-format.ts
+++ src/tasks/prettier-format.ts
@@ -1,14 +1,16 @@
 import { execa } from "execa";
+import path from "path";
 
 export async function prettierFormat(targetDir: string) {
   try {
-    const result = await execa("yarn", ["format"], { cwd: targetDir });
+    const nextjsPath = path.join(targetDir, "packages/nextjs");
+    const result = await execa("yarn", ["format"], { cwd: nextjsPath });
 
     if (result.failed) {
       throw new Error("There was a problem running the format command");
     }
   } catch (error) {
-    throw new Error("Failed to create directory", { cause: error });
+    throw new Error("Failed to format Next.js project", { cause: error });
   }
 
   return true;
