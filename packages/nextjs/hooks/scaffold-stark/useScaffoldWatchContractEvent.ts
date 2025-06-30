0c54a883937a44ec51ba192f507188d705b0ee86
OWK50GA <okoriewifrid.20201206203@futo.edu.ng>
2025-06-30 16:35:31 +0100

    ran yarn format

--- packages/nextjs/hooks/scaffold-stark/useScaffoldWatchContractEvent.ts
+++ packages/nextjs/hooks/scaffold-stark/useScaffoldWatchContractEvent.ts
@@ -12,7 +12,12 @@ import { useDeployedContractInfo } from "./useDeployedContractInfo";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { useProvider } from "@starknet-react/core";
 import { useTargetNetwork } from "./useTargetNetwork";
-import { CallData, hash, RpcProvider, events as starknetEvents } from "starknet";
+import {
+  CallData,
+  hash,
+  RpcProvider,
+  events as starknetEvents,
+} from "starknet";
 import { useInterval } from "usehooks-ts";
 import { devnet } from "@starknet-react/chains";
 import scaffoldConfig from "~~/scaffold.config";
@@ -102,14 +107,23 @@ export const useScaffoldWatchContractEvent = <
 
       const logs = rawResponseObject.events;
 
-      const eventsArray = []
+      const eventsArray = [];
       for (const log of logs) {
         eventsArray.push({
           event,
           log: log,
-          block: log.block_hash !== null ? await publicClient.getBlockWithTxHashes(log.block_hash) : null,
-          transaction: log.transaction_hash !== null ? await publicClient.getTransactionByHash(log.transaction_hash) : null,
-          receipt: log.transaction_hash !== null ? await publicClient.getTransactionReceipt(log.transaction_hash) : null,
+          block:
+            log.block_hash !== null
+              ? await publicClient.getBlockWithTxHashes(log.block_hash)
+              : null,
+          transaction:
+            log.transaction_hash !== null
+              ? await publicClient.getTransactionByHash(log.transaction_hash)
+              : null,
+          receipt:
+            log.transaction_hash !== null
+              ? await publicClient.getTransactionReceipt(log.transaction_hash)
+              : null,
         });
       }
 
@@ -119,22 +133,21 @@ export const useScaffoldWatchContractEvent = <
           log,
           starknetEvents.getAbiEvents(deployedContractData?.abi),
           CallData.getAbiStruct(deployedContractData?.abi),
-          CallData.getAbiEnum(deployedContractData?.abi)
+          CallData.getAbiEnum(deployedContractData?.abi),
         );
 
-        const args = parsed.length && parsed[0][eventName]
-        ? parsed[0][eventName]
-        : {};
+        const args =
+          parsed.length && parsed[0][eventName] ? parsed[0][eventName] : {};
         const { event: rawEvent, ...rest } = event;
-        
+
         const responseObject = {
           type: rawEvent.type,
           args,
           parsedArgs: parseEventData(args, rawEvent.name as any),
           ...rest,
-        }
+        };
         onLogs(responseObject);
-      } 
+      }
       // if (responseObject?.events?.length) {
       // }
     } catch (err: any) {
