f8682cd1ac4cfc9185df0163c2edca15540181e8
tu11aa <thanhtuhr01@gmail.com>
2025-10-07 16:50:03 +0700

    refactor: optimize BlockieAvatar component by removing unnecessary state and effect hooks

--- packages/nextjs/components/scaffold-stark/BlockieAvatar.tsx
+++ packages/nextjs/components/scaffold-stark/BlockieAvatar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { blo } from "blo";
-import { useState, useEffect } from "react";
 
 interface BlockieAvatarProps {
   address: string;
@@ -10,24 +9,7 @@ interface BlockieAvatarProps {
 
 // Custom Avatar for RainbowKit
 export const BlockieAvatar = ({ address, size }: BlockieAvatarProps) => {
-  const [avatarSrc, setAvatarSrc] = useState<string>("");
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const avatarUrl = blo(address as `0x${string}`);
-    setAvatarSrc(avatarUrl);
-    setIsLoading(false);
-  }, [address]);
-
-  if (isLoading) {
-    return (
-      <div
-        className="rounded-full bg-gray-200 dark:bg-gray-700 animate-pulse"
-        style={{ width: size, height: size }}
-      />
-    );
-  }
-
+  const avatarSrc = blo(address as `0x${string}`);
   return (
     // Don't want to use nextJS Image here (and adding remote patterns for the URL)
     // eslint-disable-next-line @next/next/no-img-element
